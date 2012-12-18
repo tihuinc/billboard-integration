f4f22585ca035cf026f64816a345dfec0270e011
Leon Hu <tianying@qualcomm.com>
2012-12-17 16:42:40 -0800

    Display proper image file name based on last sighting

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,13 +1,12 @@
 class StatusController < ApplicationController
   def billboard_1
     sighting = Sighting.where(:transmitter_name => 'transmitter_1').order("created_at DESC").first
-    if (sighting)
-      if (Time.now - sighting.created_at < 5)
-        if sighting.receiver_name == 'receiver_1'
-          payload = {:image => "tx_1_rx_1_ipad"}
-        elsif sighting.receiver_name == 'receiver_2'
-          payload = {:image => "tx_1_rx_2_ipad"}
-        end
+    if sighting && (Time.now - sighting.created_at < 10)
+      if sighting.receiver_name == 'receiver_1'
+        payload = {:image => "tx_1_rx_1_ipad.png"}
+      elsif sighting.receiver_name == 'receiver_2'
+        payload = {:image => "tx_1_rx_2_ipad.png"}
+      end
     else
       payload = {}
     end
@@ -17,17 +16,18 @@ class StatusController < ApplicationController
 
   def billboard_2
     sighting = Sighting.where(:transmitter_name => 'transmitter_2').order("created_at DESC").first
-    if (sighting)
-      if (Time.now - sighting.created_at < 5)
-        if sighting.receiver_name == 'receiver_1'
-          payload = {:image => "tx_2_rx_1_ipad"}
-        elsif sighting.receiver_name == 'receiver_2'
-          payload = {:image => "tx_2_rx_2_ipad"}
-        end
+    if sighting && (Time.now - sighting.created_at < 10)
+      if sighting.receiver_name == 'receiver_1'
+        payload = {:image => "tx_2_rx_1_ipad.png"}
+      elsif sighting.receiver_name == 'receiver_2'
+        payload = {:image => "tx_2_rx_2_ipad.png"}
+      end
     else
       payload = {}
     end
 
     return render json: payload.to_json, status: :ok
+    
   end
+
 end
